6a5586b1b257fa7d1d9228375c1b6f6b3a207165
wenjing <watersky89@126.com>
2012-09-15 13:11:03 -0500

    Fix bug when wrong number of arguments
    Add arguments check. Exit if wrong number of args.

--- hello.rb
+++ hello.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby
 
+# Check number of arguments
+unless ARGV.length == 2
+  puts "Wrong number of arguments. Exiting..."
+  exit
+end
+
 name = ARGV[0].to_s
 count = ARGV[1].to_i
 
